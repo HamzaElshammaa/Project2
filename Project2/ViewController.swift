55d8c0ff8b65a75fbf1f611f8182a58b06cd7181
Hamza <mozamiza@gmail.com>
2024-07-21 14:00:52 +0300

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Hamza Hatem on 21/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
